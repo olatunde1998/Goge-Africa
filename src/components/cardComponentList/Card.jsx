388b33563a3835d6c666b20870685c9682c2a2fc
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-11-28 15:27:46 +0100

    Card styling in card.jsx  was edited

--- src/components/cardComponentList/Card.jsx
+++ src/components/cardComponentList/Card.jsx
@@ -1,72 +1,68 @@
-import React from 'react'
-import styled, { css } from 'styled-components';
-import ImageComponent from 'components/ImageComponents/ImageComponent';
-import { Typography } from 'components/Typography';
-import Button from 'components/buttonComponent/Button';
+import React from "react";
+import styled, { css } from "styled-components";
+import ImageComponent from "components/ImageComponents/ImageComponent";
+import { Typography } from "components/Typography";
+import  { ButtonContainer } from "components/buttonComponent/Button";
 
 const CardContainer = styled.div`
-    max-width: 30rem;
-    margin: 20px 10px 30px 10px; 
-    @media (min-width: 768px) {
+  max-width: 30rem;
+  margin: 20px 10px 30px 10px;
+  @media (min-width: 768px) {
     margin-left: auto;
     margin-right: auto;
-    }
+  }
 `;
 const ImageContainer = styled.div`
-    height: 50%;
-    background-color: #FED84F;
-    margin:none;
-    border-radius: 15px 15px 0px 0px;
+  height: 50%;
+  background-color: #fed84f;
+  margin: none;
+  border-radius: 15px 15px 0px 0px;
 
-    ${(props) => {
-        switch (props.mode) {
-          case "primary":
-            return css`
-              background-color: #EBEBED;
-              color: white;
-
-            `;
-          case "secondary":
-            return css`
-                background-color: #D0D0D2;
-                color: white;
-                `;  
-          default:
-            return css`
-              background-color: #E0B9C8;
-              color: black;
-            `;
-        }
-      }}
-
-
+  ${(props) => {
+    switch (props.mode) {
+      case "primary":
+        return css`
+          background-color: #ebebed;
+          color: white;
+        `;
+      case "secondary":
+        return css`
+          background-color: #d0d0d2;
+          color: white;
+        `;
+      default:
+        return css`
+          background-color: #e0b9c8;
+          color: black;
+        `;
+    }
+  }}
 `;
 const DetailContainer = styled.div`
-    border: 2px solid #FF8A00;
-    text-align: left;
-    background-color: #FF8A00;
-    color: white;
-    height: 50%;
-    padding-left: 10px;
-    border-radius: 0px 0px 15px 15px;  
+  border: 2px solid #ff8a00;
+  text-align: left;
+  background-color: #ff8a00;
+  color: white;
+  height: 50%;
+  padding-left: 10px;
+  padding-bottom: 15px;
+  border-radius: 0px 0px 15px 15px;
 `;
 
-
-
-function card({imgURL,description,date, headings, bg}) {
+function card({ imgURL, description, date, headings, bg }) {
   return (
     <CardContainer>
-        <ImageContainer mode={bg}>
-            <ImageComponent src={imgURL}/>
-        </ImageContainer>
-        <DetailContainer>
-            <Typography variant="bodyLarge">{headings}</Typography>
-            <Typography variant="bodySmall">{date}</Typography>
-            <Typography variant="bodySmall">{description}</Typography>
-            <Button>Read More...</Button>
-        </DetailContainer>
+      <ImageContainer mode={bg}>
+        <ImageComponent src={imgURL} />
+      </ImageContainer>
+      <DetailContainer>
+        <Typography variant="bodyLarge">{headings}</Typography>
+        <Typography variant="bodySmallTwo">{date}</Typography>
+        <Typography variant="bodySmallTwo">{description}</Typography>
+        <ButtonContainer primary>Read More...</ButtonContainer>
+      </DetailContainer>
     </CardContainer>
-  )
+  );
 }
 
-export default card
+export default card;
