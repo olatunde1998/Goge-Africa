3b1489d72303a7b8b3bc345ab9298d0c956127e6
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-11-28 15:23:27 +0100

    card button was fixed

--- src/components/buttonComponent/Button.jsx
+++ src/components/buttonComponent/Button.jsx
@@ -1,21 +1,22 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const ButtonContainer = styled.button`
-    padding: .6rem 2rem;
-    min-width: 120px;
-    border-radius: 2rem;
-    background: #FF8A00;
-    color: white;
-    display: block;
-    border: none;
-    text-decoration: none;
-    text-align: center;
+export const ButtonContainer = styled.button`
+
+background: ${props => props.primary ? "white" : "#FF8A00"};
+  color: ${props => props.primary ? "#FF8A00" : "white"};
+
+  background-color: "#FF8A00";
+  color: "white";
+  padding: 0.6rem 2rem;
+  min-width: 120px;
+  border-radius: 2rem;
+  display: block;
+  border: none;
+  text-decoration: none;
+  text-align: center;
 `;
 
-
-function Button({children}) {
-  return (
-    <ButtonContainer>{children}</ButtonContainer>
-  )
+function Button({ children }) {
+  return <ButtonContainer>{children}</ButtonContainer>;
 }
 export default Button;
