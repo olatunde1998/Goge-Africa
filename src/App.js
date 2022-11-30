e3a55bf6346dadcb8a382311319f16e4d9d56d93
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-11-30 08:11:24 +0100

    create contacUsPag.jsx and rendering it to root file: App.jsx as a route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import LoginPage from 'pages/LoginPage';
 import Blog from 'pages/Blog';
+import ContactUsPage from 'pages/ContactUsPage';
 import {Routes, Route} from "react-router-dom"
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element = {<LoginPage/>} />
         <Route path="blog" element = {<Blog />}/>
-        <Route path="loginPage" element = {<LoginPage/>} />
+        <Route path="contactuspage" element = {<ContactUsPage/>} />
+        
       </Routes>
     </div>
   );
