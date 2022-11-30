d500f5ca1ca1660006da85b9a37e12e4a47e0511
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-11-30 08:18:44 +0100

    added LargeButtonContainer component as export component   to Button.jsx

--- src/components/buttonComponent/Button.jsx
+++ src/components/buttonComponent/Button.jsx
@@ -1,19 +1,26 @@
 import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
-
-background: ${props => props.primary ? "white" : "#FF8A00"};
-  color: ${props => props.primary ? "#FF8A00" : "white"};
-
+  background: ${(props) => (props.primary ? "white" : "#FF8A00")};
+  color: ${(props) => (props.primary ? "#FF8A00" : "white")};
+  
   background-color: "#FF8A00";
   color: "white";
   padding: 0.6rem 2rem;
   min-width: 120px;
-  border-radius: 2rem;
+  border-radius: 1.8rem;
   display: block;
   border: none;
   text-decoration: none;
   text-align: center;
+`;
+export const LargeButtonContainer = styled(ButtonContainer)`
+  min-width: 26rem;
+  padding: 1.3rem 2.2rem;
+  border-radius: 1.6rem;
+  @media (max-width: 750px) {
+    min-width: 18rem;
+  }
 `;
 
 function Button({ children }) {
