0dcde7bb647c8822b82c2876bd5cbe6600621671
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-12-09 08:18:06 +0100

    unwanted stying and component  were deleted

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -1,64 +1,15 @@
 import styled from "styled-components";
-import { Typography } from "components/Typography";
-import NavigationComponent from "components/navigationComponents/NavigationComponent";
-import FooterComponent from "components/footerComponents/FooterComponent";
-import AdvertComponent from "components/heroComponents/HeroComponent";
-import InputFieldComponent, {
-  Input,
-} from "components/inputFieldComponents/InputFieldComponents";
-import { LargeButtonContainer } from "components/buttonComponent/Button";
+import ContactUsComponent from "components/contactUsComponent/ContactUsComponent";
 
-const MessageFormContainer = styled.div`
-  text-align: center;
-  padding: 5rem 0 10rem 0;
-`;
-const ButtonWrapper = styled.div`
-  margin-top: 2rem;
-  display: flex;
-  justify-content: center;
-`;
-const LetTalkWrapper = styled.div`
-  padding: 0 20px 0 2rem;
-  margin: 0 auto;
-  @media (min-width: 768px) {
-    padding: 0rem;
-    max-width: 24rem;
-  }
-`;
-const InputWrapper = styled.div`
-  max-width: 768px;
-  margin: 0 auto;
-`;
+const ContactUsPageWrapper = styled.div`
+
+`
+
 function ContactUsPage() {
   return (
-    <div>
-      <NavigationComponent />
-      <AdvertComponent
-        title="24hrs On Deck Customer Support"
-        subtitle="We are at your call. We serve you always."
-        imgURL="/assets/img/contactPage-img.png"
-      />
-      <MessageFormContainer>
-        <LetTalkWrapper>
-          <Typography variant="bodyMedium">Let's Talk!</Typography>
-          <Typography variant="bodySmallThree">
-            We do Normally get back within 48hours. Please leave a message.
-          </Typography>
-        </LetTalkWrapper>
-        <InputWrapper>
-          <InputFieldComponent placeholder="FullName" />
-          <InputFieldComponent placeholder="Country/Region" />
-          <InputFieldComponent placeholder="Street Address" />
-          <InputFieldComponent placeholder="City" />
-          <InputFieldComponent placeholder="Your Message" />
-          <Input heighty placeholder="textarea" />
-        </InputWrapper>
-        <ButtonWrapper>
-          <LargeButtonContainer>Send Message</LargeButtonContainer>
-        </ButtonWrapper>
-      </MessageFormContainer>
-      <FooterComponent />
-    </div>
+    <ContactUsPageWrapper>
+      <ContactUsComponent/>
+    </ContactUsPageWrapper>
   );
 }
 
