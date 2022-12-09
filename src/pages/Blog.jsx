9088a5b49e117df3df4340cd0fd74c912aed4365
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-12-09 08:16:53 +0100

    new tag element was added - HeroRedComponent

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,5 @@
 import NavigationComponent from "components/navigationComponents/NavigationComponent";
-import HeroComponent from "components/heroComponents/HeroComponent";
+import HeroRedComponent from "components/heroRedComponents/HeroRedComponent";
 import FooterComponent from "components/footerComponents/FooterComponent";
 import CardComponentList from "components/cardComponentList/CardComponentList";
 import DummyData from "components/dummyData/cardData.json";
@@ -8,7 +8,7 @@ function Blog() {
   return (
     <div>
       <NavigationComponent />
-      <HeroComponent
+      <HeroRedComponent
         title="Welcome to Our Forever Updated Blog Post!"
         subtitle="Be informed! Never miss a Single post"
         imgURL="/assets/img/blogPage-img.png"
