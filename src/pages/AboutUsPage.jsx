7977bd05a905a300d7da8e165b055d4d46c6788c
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-12-09 08:15:32 +0100

    unwanted tag element and import component  was deleted

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,19 +1,14 @@
-import React from "react";
-import NavigationComponent from "components/navigationComponents/NavigationComponent";
-import HeroComponent from "components/heroComponents/HeroComponent";
-import FooterComponent from "components/footerComponents/FooterComponent";
+import AboutUsComponent from "components/aboutUsComponent/AboutUsComponent";
+import styled from "styled-components";
+
+const AboutUsPageWrapper = styled.div`
+`
 
 function AboutUsPage() {
   return (
-    <div>
-      <NavigationComponent />
-      <HeroComponent
-        title="We're Goge Africa An E-Learning Platform From West Africa"
-        subtitle="We are at your call. We serve you always."
-        imgURL="assets/img/about-us-Image.png"
-      />
-      <FooterComponent />
-    </div>
+    <AboutUsPageWrapper>
+      <AboutUsComponent />
+    </AboutUsPageWrapper>
   );
 }
 
