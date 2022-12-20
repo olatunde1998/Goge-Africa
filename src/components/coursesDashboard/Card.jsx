4bb0d7923af2432b89ece7aaeb1aad4e4978a701
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-12-20 10:29:32 +0100

    4 files was created -- card cardComponent courseDashboard OrderDashboard

---
+++ src/components/coursesDashboard/Card.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import ImageComponent from "components/imageComponents/ImageComponent";
+import { Typography } from "components/Typography";
+
+
+const CardContainer = styled.div`
+  width: 150px;
+  margin: 2px 5px 20px 2px;
+  @media (min-width: 768px) {
+  width: 200px;  
+  margin: 20px 2px 10px 20px;
+
+  }
+`;
+const ImageContainer = styled.div`
+  height: auto;
+  background-color: #E5E5E5;
+  margin: none;
+  border-radius: 15px 15px 0px 0px;
+`;
+const DetailContainer = styled.div`
+  border: 2px solid #ff8a00;
+  text-align: left;
+  background-color: #ff8a00;
+  color: white;
+  height:40%;
+  padding-left: 20px;
+  padding-right: 30%;
+  padding-bottom: 15px;
+  border-radius: 0px 0px 15px 15px;
+`;
+
+function Card({ imgURL, courseName }) {
+  
+  return (
+    <CardContainer>
+      <ImageContainer>
+        <ImageComponent src={imgURL} />
+      </ImageContainer>
+
+      <DetailContainer>
+        <Typography variant="bodySmallTwo">{courseName}</Typography>
+      </DetailContainer>
+    </CardContainer>
+  );
+}
+
+export default Card;
---
+++ src/components/coursesDashboard/CardComponentList.jsx
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import Card from "components/coursesDashboard/Card";
+// import axios from "axios";
+// import { useState } from "react";
+import { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchCourses } from "redux/actions/CoursesAction";
+
+
+const CardComponentListWrapper = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  @media (min-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 820px;
+  }
+`;
+
+function CardComponentList({dispatch, coursesData, loading, hasError}) {
+  useEffect(() => {
+    dispatch(fetchCourses())
+  }, [dispatch])
+  
+  if(loading) return <p>Loading Courses ......</p>
+  if(hasError)return<p>Unable to get the courses</p>
+  return (
+    <CardComponentListWrapper>
+      {coursesData &&
+        coursesData.data &&
+        coursesData.data.map((course, idx) => {
+          const {attributes: {imageUrl: {data: {attributes: {url}}}}} = course
+          console.log(course.attributes.name);
+          return (
+            <Card
+              key={idx}
+              imgURL={`http://localhost:1337${url}`}
+              courseName={course.attributes.name}
+            />
+          );
+        })}
+    </CardComponentListWrapper>
+  );
+}
+
+
+const mapStateToProps = (state) => ({
+  coursesData: state.courses.courses,
+  loading: state.courses.loading,
+  hasError: state.courses.hasError,
+});
+
+export default connect(mapStateToProps)(CardComponentList) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function CardComponentList() {
+//   function courseData() {
+//     axios
+//       .get("http://localhost:1337/api/courses/?populate=*")
+//       .then((response) => {
+//         console.log(response.data);
+//         setCourses(response.data);
+//       });
+//   }
+//   const [courses, setCourses] = useState({});
+//   useEffect(() => {
+//     courseData();
+//   }, []);
+//   return (
+//     <CardComponentListWrapper>
+//       {courses &&
+//         courses.data &&
+//         courses.data.map((course, idx) => {
+//           const {attributes: {imageUrl: {data: {attributes: {url}}}}} = course
+//           console.log(course.attributes.name);
+//           return (
+//             <Card
+//               key={idx}
+//               imgURL={`http://localhost:1337${url}`}
+//               courseName={course.attributes.name}
+//             />
+//           );
+//         })}
+//     </CardComponentListWrapper>
+//   );
+// }
+
+
+
+// export default CardComponentList;
