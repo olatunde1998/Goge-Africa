a1bd4cca497d01e79e2fd5b325b11c4ebd7ce75d
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-12-20 10:46:42 +0100

    added CourseDashboard component was done

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,12 +1,14 @@
 import NavigationComponent from "components/navigationComponents/NavigationComponent";
 import FooterComponent from "components/footerComponents/FooterComponent";
 import HeroComponent from "components/heroComponents/HeroComponent";
+import CourseDashboard from "components/coursesDashboard/CourseDashboard";
 
 function Courses() {
   return (
     <div>
       <NavigationComponent />
       <HeroComponent />
+      <CourseDashboard/>
       <FooterComponent />
     </div>
   );
