5f6b30c8b1e22040e9a0e07302bc34b2137d6906
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-12-20 10:22:14 +0100

    fontawesome was imported(added)  to the component

--- src/components/aboutUsComponent/TestimonyComponent.jsx
+++ src/components/aboutUsComponent/TestimonyComponent.jsx
@@ -1,40 +1,43 @@
 import styled from "styled-components";
 import Testimony from "components/aboutUsComponent/Testimony";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faAward, faFile, faUserGraduate, faEye } from "@fortawesome/free-solid-svg-icons";
 
-const TestimonyWrapper = styled.div`
+const TestimonyComponentWrapper = styled.div`
   margin-top: 50px;
   display: flex;
   justify-content: space-around;
   background-color: #ff8a00;
+  padding: 60px 20px 60px 30px;
+
 `;
 
 function TestimonyComponent() {
   return (
-    <TestimonyWrapper>
-      <FontAwesomeIcon Icon={faYoutube}></FontAwesomeIcon>
+    <TestimonyComponentWrapper>
       <Testimony
         rating="5,679"
         comment="Registered Students"
-        imgURL={faGithub}
-      ></Testimony>
+        icon={faUserGraduate}
+      />
+
       <Testimony
         rating="2,679"
-        comment="Students has been helped to achieve their dreams"
-        imgURL={{ faYoutube }}
+        comment="Students has been helped to their dreams"
+        icon={faFile}
+
       />
       <Testimony
         rating="10,000"
-        comment="More than 10,000 people visits our site monthly"
-        imgURL={{ faYoutube }}
+        comment="More than 10,000 people visits our site "
+        icon={faEye}
+
       />
       <Testimony
         rating="#10"
-        comment="Ranked Among the top 10 growing online learning startup in West Africa"
-        imgURL={{ faYoutube }}
+        comment="Ranked Among the top 10 growing  Africa"
+        icon={faAward}
       />
-    </TestimonyWrapper>
+    </TestimonyComponentWrapper>
   );
 }
 
