9f11e43c7323ca1c9ded2171935c54c7cb6652a7
Olatunde Rasheed <olatunderasheed@Olatundes-MacBook-Air.local>
2022-12-21 21:52:08 +0100

    components content were formatted with prettier vscode

--- src/redux/reducers/blogsReducer.js
+++ src/redux/reducers/blogsReducer.js
@@ -1,20 +1,24 @@
-import { GET_BLOGS, GET_BLOGS_SUCCESS, GET_BLOGS_FAILURE } from "redux/actions/blogAction";
+import {
+  GET_BLOGS,
+  GET_BLOGS_SUCCESS,
+  GET_BLOGS_FAILURE,
+} from "redux/actions/blogAction";
 
 export const initialState = {
-    blogs: [],
-    loading: false,
-    hasError: false
-}
+  blogs: [],
+  loading: false,
+  hasError: false,
+};
 
-export default function blogsReducer (state= initialState, action){
-    switch(action.type){
-        case GET_BLOGS:
-            return {...state, loading: true}
-        case GET_BLOGS_SUCCESS:
-            return {blogs: action.payload, loading:false, hasError: false}
-        case GET_BLOGS_FAILURE:
-            return {...state, loading: false, hasError: true}
-        default:
-            return state;
-    }
+export default function blogsReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_BLOGS:
+      return { ...state, loading: true };
+    case GET_BLOGS_SUCCESS:
+      return { blogs: action.payload, loading: false, hasError: false };
+    case GET_BLOGS_FAILURE:
+      return { ...state, loading: false, hasError: true };
+    default:
+      return state;
+  }
 }
--- src/redux/rootReducer/rootReducer.js
+++ src/redux/rootReducer/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import blogsReducer from 'redux/reducers/blogsReducer'
 import coursesReducer from 'redux/reducers/CoursesReducer';
+import successfulOrderReducer from 'redux/reducers/successfulOrderReducer';
 
 const rootReducer = combineReducers({
   blogs: blogsReducer,
   courses: coursesReducer,
+  successfulData: successfulOrderReducer,
 })
 
 export default rootReducer;
